48f1864da37139cbf71b9e2f14a468ea7738ec3d
Filmote <simon@bloggingadeadhorse.com>
2020-02-01 22:01:04 +1100

    Change coordinates

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2019 Hannu Viitala
 // 
 // The source code in this file is released under the MIT license.
-// Go to http://opensource.org/licenses/MIT for the full license details.5
+// Go to http://opensource.org/licenses/MIT for the full license details.
 // 
 // Converted to C++ and extended by Filmote
 
@@ -19,42 +19,252 @@ namespace Constants {
     const uint8_t tileWidth = 16;
     const uint8_t tileHeight = 16;
     
-    const uint8_t mapTileWidth = 16;                            // Map width in tiles ..
-    const uint8_t mapTileHeight = 16;                           // Map height in tiles ..
+    const uint8_t mapTileWidth = 16;                              // Map width in tiles ..
+    const uint8_t mapTileHeight = 16;                             // Map height in tiles ..
     
-    const uint16_t mapWidth = mapTileWidth * tileWidth;         // 16 tiles of 16 pixels
-    const uint16_t mapHeight = mapTileHeight * tileHeight;      // 16 tiles of 16 pixels
+    const uint16_t worldWidth = mapTileWidth * tileWidth;         // 16 tiles of 16 pixels
+    const uint16_t worldHeight = mapTileHeight * tileHeight;      // 16 tiles of 16 pixels
+    
+    const uint16_t screenCentreX = PD::width / 2; 
+    const uint16_t screenCentreY = PD::height / 2;
 
 }
+
 
 
 // ---------------------------------------------------------------------------------------
 
 
 enum TileType {
-
+    
 	Water = 0,
 	Green = 1,
 	Tree = 2,
 	Grass = 3,
-    
+
 }; 
+
+
+enum Direction {
+    
+	Up,
+	Down,
+	Left,
+	Right
+	
+}; 
+
+
+struct Entity {
+    
+    int16_t x;
+    int16_t y;
+
+    const uint8_t width = 12;
+    const uint8_t height = 15;
+
+};
 
 
 // ---------------------------------------------------------------------------------------
 
 
 Tilemap tilemap;
+Entity player;
 
 
 // ---------------------------------------------------------------------------------------
 
+
+void calculateViewPortPosition(Entity &entity, int16_t &xViewPort, int16_t &yViewPort) {
+    
+    if (entity.x < PD::width / 2) {
+        
+        xViewPort = 0;
+        
+    }
+    else if (entity.x > Constants::worldWidth - PD::width / 2) {
+
+        xViewPort = PD::width - Constants::worldWidth;
+        
+    }
+    else {
+        
+        xViewPort = PD::width / 2 - entity.x;
+
+    }
+    
+    if (entity.y < PD::height / 2) {
+        
+        yViewPort = 0;
+        
+    }
+    else if (entity.y > Constants::worldHeight - PD::height / 2) {
+
+        yViewPort = PD::height - Constants::worldHeight;
+        
+    }
+    else {
+        
+        yViewPort = PD::height / 2 - entity.y;
+
+    }
+    
+}
+
+
+// ---------------------------------------------------------------------------------------
+
+
+void calculatePlayerPosition(Entity &entity, int16_t &xPlayer, int16_t &yPlayer) {
+    
+    if (entity.x < PD::width / 2) {
+        
+        xPlayer = entity.x;
+        
+    }
+    else if (entity.x > Constants::worldWidth - PD::width / 2) {
+
+        xPlayer = entity.x - (Constants::worldWidth - PD::width);
+        
+    }
+    else {
+        
+        xPlayer = PD::width / 2;
+
+    }
+    
+    if (entity.y < PD::height / 2) {
+        
+        yPlayer = entity.y;
+        
+    }
+    else if (entity.y > Constants::worldHeight - PD::height / 2) {
+
+        yPlayer = entity.y - (Constants::worldHeight - PD::height);
+        
+    }
+    else {
+        
+        yPlayer = PD::height / 2;
+
+    }
+
+}
+
+
+// ---------------------------------------------------------------------------------------
+//
+//  Check to see if the move the entity is about to make is into a green (empty) tile.  As 
+//  the player is 12 x 15 pixels and the tiles are bigger (16 x 16) its possible that the 
+//  player could be straddling two tiles in either direction ..
+//
+bool checkMovement(Entity &entity, int16_t x, int16_t y, Direction direction) {
+
+    int8_t tileId1 = 0;
+    int8_t tileId2 = 0;
+
+    switch (direction) {
+        
+        case Direction::Left:
+            tileId1 = tilemap.GetTileId(x, y, 16);
+            tileId2 = tilemap.GetTileId(x, y + entity.height, 16);
+            break;
+        
+        case Direction::Right:
+            tileId1 = tilemap.GetTileId(x + entity.width, y, 16);
+            tileId2 = tilemap.GetTileId(x + entity.width, y + entity.height, 16);
+            break;
+
+        case Direction::Up:
+            tileId1 = tilemap.GetTileId(x, y, 16);
+            tileId2 = tilemap.GetTileId(x + entity.width, y, 16);
+            break;
+        
+        case Direction::Down:
+            tileId1 = tilemap.GetTileId(x, y + player.height, 16);
+            tileId2 = tilemap.GetTileId(x + entity.width, y + entity.height, 16);
+            break;
+            
+    }
+    
+
+
+    // If either tile is not green, do not move.
+    
+    if (tileId1 != TileType::Green || tileId2 != TileType::Green) {
+        return false;
+        
+    }
+
+    return true;
+        
+}
+
+
+// ---------------------------------------------------------------------------------------
+//
+//  Handle the player movements ..
+//
+void handlePlayerMovements() {
+
+    if (PC::buttons.pressed(BTN_LEFT) || PC::buttons.repeat(BTN_LEFT, 1))    { 
+
+        if (player.x > 0 && checkMovement(player, player.x - 1, player.y, Direction::Left)) {
+
+            player.x--;
+
+        }
+
+    }
+    
+    if (PC::buttons.pressed(BTN_RIGHT) || PC::buttons.repeat(BTN_RIGHT, 1))   { 
+
+        if (player.x < Constants::worldWidth && checkMovement(player, player.x + 1, player.y, Direction::Right)) {
+
+            player.x++;
+        
+            
+        }
+
+    }
+    
+    
+    if (PC::buttons.pressed(BTN_UP) || PC::buttons.repeat(BTN_UP, 1))      { 
+
+        if (player.y > 0 && checkMovement(player, player.x, player.y - 1, Direction::Up)) {
+
+            player.y--;
+
+        }
+        
+    }
+    
+    if (PC::buttons.pressed(BTN_DOWN) || PC::buttons.repeat(BTN_DOWN, 1))    { 
+
+        if (player.y < Constants::worldHeight && checkMovement(player, player.x, player.y + 1, Direction::Down)) {
+
+            player.y++;
+            
+        }
+
+    }
+    
+}
+
+
+
+// ---------------------------------------------------------------------------------------
+
+
 int main(){
 
     PC::begin();
+    PD::setFont(fontC64);
     PD::loadRGBPalette(palettePico);   
     PD::persistence = true;
     PD::invisiblecolor = 12;
+
 
 
     // Initialise the map ..
@@ -65,16 +275,12 @@ int main(){
     tilemap.tiles[TileType::Grass] = Data::grass16;
     tilemap.tiles[TileType::Water] = Data::water16;
 
+
+    // Position the player into a vacant spot on the map ..
     
-    int16_t x = -20;
-    int16_t y = -50;
-
-    int16_t xOffset = 0;
-    int16_t yOffset = 0;
-
-    int16_t screenCentreX = PD::width / 2; 
-    int16_t screenCentreY = PD::height / 2;
-
+    player.x = 48;
+    player.y = 48;
+    
     
     while (PC::isRunning()) {
         
@@ -83,141 +289,28 @@ int main(){
         PC::buttons.pollButtons();        
 
 
-        // Capture the old coordinates in case we need to move back ..
+
+        // Handle player movements ..
+
+        handlePlayerMovements();
+
+
+        // Render screen ..
+
+        int16_t xViewPort;
+        int16_t yViewPort;
         
-        int16_t oldX = x;
-        int16_t oldY = y;
-        int16_t oldXOffset = xOffset;
-        int16_t oldYOffset = yOffset;
-
-
-        if (PC::buttons.pressed(BTN_LEFT) || PC::buttons.repeat(BTN_LEFT, 1))    { 
-
-
-            // If we are already on the right hand side of the screen, come back to the centre ..
-            
-            if (xOffset < 0) {                                  
-                xOffset++;
-            }
-            
-            
-            // Otherwise scroll the screen itself if we can ..
-            
-            else if (x < 0) {
-                x++;
-            }
-            
-            
-            // If we cannot scroll the screen, move the player to the left ..
-            
-            else if (x <= (PD::width / 2)) {     
-                xOffset++;
-            }
-
-
-        }
-        
-        if (PC::buttons.pressed(BTN_RIGHT) || PC::buttons.repeat(BTN_RIGHT, 1))   { 
-
-
-            // If we are already on the left hand side of the screen, come back to the centre ..
-            
-            if (xOffset > 0) {
-                xOffset--;
-            }
-                 
-            
-            // Otherwise scroll the screen itself if we can ..
-            
-            else if (x > (PD::width - Constants::mapWidth) && x <= 0) {
-                x--;
-            }
-            
-            
-            // If we cannot scroll the screen, move the player to the right ..
-            
-            else if (x >= (PD::width - Constants::mapWidth)) {
-                xOffset--;
-            }
-
-            
-        }
-        
-        
-        if (PC::buttons.pressed(BTN_UP) || PC::buttons.repeat(BTN_UP, 1))      { 
-
-            
-            // If we are already in the bottom section of the screen, come back to the centre ..
-            
-            if (yOffset < 0) {                                  
-                yOffset++;
-            }
-            
-            
-            // Otherwise scroll the screen itself if we can ..
-            
-            else if (y < 0) {
-                y++;
-            }
-            
-            
-            // If we cannot scroll the screen, move the player up ..
-            
-            else if (y <= (PD::height / 2)) {     
-                yOffset++;
-            }
-            
-            
-        }
-        
-        if (PC::buttons.pressed(BTN_DOWN) || PC::buttons.repeat(BTN_DOWN, 1))    { 
-
-
-            // If we are already in the top section of the screen, come back to the centre ..
-            
-            if (yOffset > 0) {
-                yOffset--;
-            }
-                 
-            
-            // Otherwise scroll the screen itself if we can ..
-            
-            else if (y > (PD::height - Constants::mapHeight) && y <= 0) {
-                y--;
-            }
-            
-            
-            // If we cannot scroll the screen, move the player down ..
-            
-            else if (y >= (PD::height - Constants::mapHeight)) {
-                yOffset--;
-            }
-           
-        }
-
-
-
-        // Check the map tile under the player.
-        
-        int16_t playerCenterInMapX = (screenCentreX + 6) - x - xOffset;
-        int16_t playerCenterInMapY = (screenCentreY + 7) - y - yOffset;
-        uint8_t tileId = tilemap.GetTileId(playerCenterInMapX, playerCenterInMapY, 16);
-
-
-        // If the tile is not green, do not move.
-        
-        if (tileId != TileType::Green) {
-            
-            x = oldX;
-            y = oldY;
-            xOffset = oldXOffset;
-            yOffset = oldYOffset;
-            
-        }
+        calculateViewPortPosition(player, xViewPort, yViewPort);
+        tilemap.draw(xViewPort, yViewPort);
 
         
-        tilemap.draw(x, y);
-        PD::drawBitmapData(screenCentreX - xOffset, screenCentreY - yOffset, 12, 15, Data::girl12x15Pixels);
+        // Render player ..
+        
+        int16_t xPlayer;
+        int16_t yPlayer;
+        
+        calculatePlayerPosition(player, xPlayer, yPlayer);
+        PD::drawBitmapData(xPlayer, yPlayer, 12, 15, Data::girl12x15Pixels);        
 
     }
     
